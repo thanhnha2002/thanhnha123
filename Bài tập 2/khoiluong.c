28c7e2c5307befbe8b677d67a813f18307d1079f
thanhnha <72533406+thanhnha2002@users.noreply.github.com>
2021-01-14 09:12:18 +0700

    Create khoiluong.c

---
+++ Bài tập 2/khoiluong.c
@@ -0,0 +1,20 @@
+  
+#include <stdio.h>
+int main(void)
+{	 
+	//khai bao bien m
+	int m;
+	//Hien thi ra man hinh
+	printf("NHAP m = ");
+	//Nhap khoi luong kg m tu ban phim
+	scanf("%d",&m);
+	//xuat ra man hinh
+	printf("Khoi luong gam = %d\n", m * 1000);
+	printf("KG   |G\n");
+	printf("----------\n");
+	printf("%d", m);
+	printf("    |%d", m * 1000);
+
+
+    return 0;
+}
